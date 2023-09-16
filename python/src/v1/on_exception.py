fd9326933e20734927eff10db49c3e9049f97728
Roma Baranenko <sommelier.jungle@gmail.com>
2023-09-16 17:16:04 +0300

    v3

--- python/src/v1/on_exception.py
+++
@@ -1,4 +0,0 @@
-def on_exception(phone_lines, uuid, peer_name, exception):
-    print(f"Call with UUID {uuid} from {peer_name} caused exception: {str(exception)}\n")
-    del phone_lines[uuid]
-    pass
